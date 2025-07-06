3057a07f9797e4a392c4490ab72521e16bba2458
srishtireddy <reddysrishti09@gmail.com>
2025-07-06 23:09:42 +0530

    Week-3 added

---
+++ WEEK-3/Exercise-1/Code/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+    public String getBookName() {
+        return "Spring in Action";
+    }
+
+    public String getAuthor() {
+        return "Craig Walls";
+    }
+}
---
+++ WEEK-3/Exercise-2/Code/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void printBook() {
+        System.out.println("Book: 'Spring in Action', Author: Craig Walls");
+    }
+}
